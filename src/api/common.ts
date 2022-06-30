6b564a701f1ae1f1e24c10d4f69370042129025a
梁傲 <1289206613@qq.com>
2022-06-30 17:45:03 +0800

    fix(api): 修改注册公共结构api参数

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,18 +1,27 @@
 import { get, post } from '@/utils/http'
 import { Result, PageSize } from './global'
 
-export function useRegisterCommonAPI<R, T>(prefix: string) {
+/**
+ *
+ *  注册公共结构的新删查改接口
+ * @export
+ * @template R 返回结果类型(data)
+ * @template P 参数类型(分页、列表)
+ * @param {string} prefix api地址模块前缀
+ * @return {*}
+ */
+export function useRegisterCommonAPI<R, P>(prefix: string) {
   type PageResult = Result<{
-    list: T[]
+    list: R[]
     total: number
   }>
   type PageData<P> = P & PageSize
   // 分页
-  const getPageList = async (params: PageData<T>) =>
+  const getPageList = async (params: PageData<P>) =>
     get<PageResult>({ url: `/${prefix}/page`, params })
 
   // 列表
-  const getList = async (params: T) => get<Result<R[]>>({ url: `/${prefix}/list`, params })
+  const getList = async (params: P) => get<Result<R[]>>({ url: `/${prefix}/list`, params })
 
   // 新增
   const create = async (params: R) => post<Result<boolean>>({ url: `/${prefix}/add`, params })
@@ -30,7 +39,7 @@ export function useRegisterCommonAPI<R, T>(prefix: string) {
 
   // 查询
   const read = async (id: string | number) =>
-    get<Result<boolean>>({ url: `/${prefix}/get`, params: { id } })
+    get<Result<R>>({ url: `/${prefix}/get`, params: { id } })
 
   // 删除
   const remove = async (id: string | number) =>
